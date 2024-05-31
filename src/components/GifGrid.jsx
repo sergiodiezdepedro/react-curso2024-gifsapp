0fc8d20653b260921024a29501f708444b7c6b27
sergiodiezdepedro <sergio.d.depedro@gmail.com>
2024-05-31 11:41:40 +0200

    Version 1.0

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({ category }) => {
+  const { images, isLoading } = useFetchGifs(category);
+
+  return (
+    <>
+      <h3>{category}</h3>
+      {isLoading && <p>Cargando...</p>}
+      <div className='card-grid'>
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
+};
